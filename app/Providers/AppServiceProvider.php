c3c42dbc6d6f8ee498a2be4b6d8a2a1c204971ba
hidesan4869 <t1123.gogo@gmail.com>
2024-07-19 16:11:59 +0900

    init

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,6 +19,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        dd(config('app.env'));
+        if (config('app.env') === "production") {
+            \URL::forceScheme('https');
+        }
     }
 }
